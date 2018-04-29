aca1e0387a6506a7b80b5ae457d538e8559b07fc
Pavan <Pavan@Pavans-MacBook-Pro.local>
2018-04-29 12:34:34 +0100

    added some modules and testing out travis ci

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/',function(req,res) {
-  res.send("Hello World");
+// viewed at http://localhost:4000
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
-
 app.listen(4000, console.log('Your app is running on port: 4000'));
